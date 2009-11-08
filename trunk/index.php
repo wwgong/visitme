967e9c821a6661c8e0bb7a2c89121ed30f305674
danorux <danorux@e39c1ee0-ad43-11de-ac05-951101a8bf3e>
2009-11-08 05:37:59 +0000

    Apptab prints stuff again!

--- trunk/index.php
+++ trunk/index.php
@@ -7,8 +7,16 @@ $facebook = new Facebook($api_key, $secret);
 $facebook->require_frame();
 $user = $facebook->require_login();
 
-$facebook->api_client->profile_setFBML(NULL, $user, 'profile', NULL, NULL, 'box_content');
+$box = '<script type="text/javascript">function update_user_box() {
+	var user_box = document.getElementById("blah");
+	
+	user_box.innerHTML = "ajax"; } </script>
+	<a onclick="update_user_box();" href="#">blah</a>
+	<div id="blah"></div>';
+
+//$facebook->api_client->profile_setFBML(NULL, $user, 'profile', NULL, NULL, 'profile_main');
 $user_details = $facebook->api_client->users_getInfo($user, 'last_name, first_name');
+$facebook->api_client->profile_setFBML(NULL, $user, 'profile', NULL, NULL, $box);
 
 $smarty = new Smarty();
 
