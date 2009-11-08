83c9be2641685cfa3891a923941623361dd1c470
danorux <danorux@e39c1ee0-ad43-11de-ac05-951101a8bf3e>
2009-11-08 05:37:59 +0000

    Apptab prints stuff again!

--- apptab.php
+++ apptab.php
@@ -4,12 +4,23 @@ require_once('includes/config.php');
 require_once('includes/smarty/libs/Smarty.class.php');
 
 $facebook = new Facebook($api_key, $secret);
-//$facebook->require_frame();
-//$user = $facebook->require_login();
-$user = $_POST['fb_sig_profile_user']; 
 
-//$blah = $facebook->api_client->profile_setFBML(NULL, $user, 'profile', NULL, NULL, 'profile_main');
-$facebook->api_client->profile_setFBML(NULL, $user, 'profile', NULL, NULL, 'profile_main');
-//echo "Hello, <fb:name/>";
-echo $blah;
+$uid1 = $_POST['fb_sig_profile_user']; 
+$uid2 = $_POST['fb_sig_user'];
+
+$smarty = new Smarty();
+$smarty->assign('uid1',$uid1);
+$smarty->assign('uid2',$uid2);
+
+$smarty->assign('flight1_cost',100);
+$smarty->assign('flight1_departure',100);
+$smarty->assign('flight1_arrival',100);
+$smarty->assign('flight1_airline',100);
+
+$smarty->assign('flight2_cost',100);
+$smarty->assign('flight2_departure',100);
+$smarty->assign('flight2_arrival',100);
+$smarty->assign('flight2_airline',100);
+
+$smarty->display('apptab.tpl');
 ?>
---
+++ templates/apptab.tpl
@@ -0,0 +1,2 @@
+You can visit <fb:name uid="{$uid1}"/> for {$flight1_cost}. <br/>
+<fb:name uid="{$uid1}"/> can visit you for {$flight2_cost}.
