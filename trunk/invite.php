6701037e9927e92a4612c2479f207af54f736130
bmoy2008 <bmoy2008@e39c1ee0-ad43-11de-ac05-951101a8bf3e>
2009-11-26 01:01:46 +0000

    Edited about.php and invite.php to use smarty.

--- trunk/invite.php
+++ trunk/invite.php
@@ -1,47 +1,30 @@
 <?php
     require_once('includes/config.php');
+
+    $appUserList = $facebook->api_client->friends_getAppUsers();
+
+    $excludeFriendList = NULL;
+
+    foreach($appUserList as $appUserId)
+    {
+        if($excludeFriendList == NULL)
+        {
+            $excludeFriendList = $excludeFriendList.$appUserId;
+        }
+        else
+        {
+            $excludeFriendList = $excludeFriendList.','.$appUserId;
+        }
+    }
+
+    $link = htmlentities("<fb:req-choice url=\"http://apps.facebook.com/visitme/\" label=\"Authorize My Application\">");
+
+    $smarty = new Smarty();
+    $smarty->assign('excludeFriendList', $excludeFriendList);
+    $smarty->assign('link', $link);
+    $smarty->display('invite.tpl');
 ?>
 
-<div style="text-align:right;">
-     <img src="http://www.kayak.com/v283/images/logos/kayak-175px-static.png" id="logo" />
-</div>
-
-<div style="text-align:center;">
-    <fb:tabs>
-        <fb:tab-item href='index.php' title='Home' selected='false'/>
-        <fb:tab-item href='invite.php' title='Invite Friends' selected='true'/>
-		<fb:tab-item href='about.php' title='About' selected='false' />
-    </fb:tabs>
-</div>
-
-<div style="text-align:center;">
-    <?php
-       $appUserList = $facebook->api_client->friends_getAppUsers();
-       $excludeFriendList = NULL;
-       foreach($appUserList as $appUserId)
-       {
-           if($excludeFriendList == NULL)
-           {
-               $excludeFriendList = $excludeFriendList.$appUserId;
-           }
-           else
-           {
-               $excludeFriendList = $excludeFriendList.','.$appUserId;
-           }
-       }
-    ?>
-
-    <fb:fbml>
-        <fb:request-form
-            action="index.php"
-            method="POST"
-            invite="true"
-            type="VisitME"
-            content="Social flight ticketing!<?php echo htmlentities("<fb:req-choice url=\"http://apps.facebook.com/kayakme/\" label=\"Authorize My Application\"") ?>" >
-            <fb:multi-friend-selector showborder="false" actiontext="Invite your friends to use VisitME." exclude_ids="<?php echo $excludeFriendList; ?>">>
-        </fb:request-form>
-    </fb:fbml>
-</div>
 
 
 
---
+++ trunk/templates/invite.tpl
@@ -0,0 +1,22 @@
+<div style="text-align:right;">
+     <img src="http://www.kayak.com/v283/images/logos/kayak-175px-static.png" id="logo" />
+</div>
+
+<div style="text-align:center;">
+    <fb:tabs>
+        <fb:tab-item href='index.php' title='Home' selected='false'/>
+        <fb:tab-item href='invite.php' title='Invite Friends' selected='true'/>
+	<fb:tab-item href='about.php' title='About' selected='false' />
+    </fb:tabs>
+
+    <fb:fbml>
+        <fb:request-form
+            action="index.php"
+            method="POST"
+            invite="true"
+            type="VisitME"
+            content="Social flight ticketing!{$link}"
+            <fb:multi-friend-selector showborder="false" actiontext="Invite your friends to use VisitME." exclude_ids={$excludeFriendList}>>
+        </fb:request-form>
+    </fb:fbml>
+</div>
