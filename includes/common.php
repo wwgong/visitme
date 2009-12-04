a6c8d619f73aeed88a470f423d1c747b03447a24
danorux <danorux@e39c1ee0-ad43-11de-ac05-951101a8bf3e>
2009-12-04 23:39:51 +0000

    Search function now finds the cheapest fare between locations on Facebook profiles on both ends, aka VisitME works for users outside Boston now.

---
+++ includes/common.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("sqlfunctions.php");
+
+/** Takes in an origin code and an array of destination codes
+ *
+ *	This function provides 1->N location fares
+ */
+function get_fares_code_to_city($origin_code, $dest_codes, $debug)
+{
+	// Create URL string
+	$rssURL = 'http://www.kayak.com/h/rss/fare?dest=';
+	for ($i = 0; $i < sizeof($dest_codes) - 1; $i++)
+	{
+		$rssURL = $rssURL.$dest_codes[$i].",";
+	}
+	$rssURL = $rssURL.$dest_codes[sizeof($dest_codes) - 1];
+	$rssURL = $rssURL.'&code='.$origin_code;
+	
+	// Get RSS feed
+	$rss	= fetch_rss($rssURL);
+	
+	if($debug)
+	{
+		//echo "<br/>Fares url: $rssURL<br/>";
+		//print_r($rss);
+		//echo "<br/><br/>";
+	}
+	
+	return $rss;
+}
+
+function get_fares_ip_to_city($ip, $destination, $debug)
+{
+
+}
+
+?>
