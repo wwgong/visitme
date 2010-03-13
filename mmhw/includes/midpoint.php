03ed377bdc618fe935430257ac515a7e96c7b420
bmoy2008 <bmoy2008@e39c1ee0-ad43-11de-ac05-951101a8bf3e>
2010-03-13 20:34:06 +0000

    minor fix: changed function set_lola() from public to private...

--- mmhw/includes/midpoint.php
+++ mmhw/includes/midpoint.php
@@ -28,31 +28,31 @@
         {
             return (array($this->get_longitude(), $this->get_latitude()));
         }
-		
+
 		/* sets longitude and latitude for the midpoint */
-        public function set_lola($lola_1, $lola_2)
+        private function set_lola($lola_1, $lola_2)
         {
             $lola_1[1] = deg2rad($lola_1[1]);
             $lola_1[0] = deg2rad($lola_1[0]);
             $lola_2[1] = deg2rad($lola_2[1]);
             $lola_2[0] = deg2rad($lola_2[0]);
-			
+
             $x1 = cos($lola_1[1]) * cos($lola_1[0]);
             $y1 = cos($lola_1[1]) * sin($lola_1[0]);
             $z1 = sin($lola_1[1]);
-			
+
             $x2 = cos($lola_2[1]) * cos($lola_2[0]);
             $y2 = cos($lola_2[1]) * sin($lola_2[0]);
             $z2 = sin($lola_2[1]);
-			
+
             $xm = ( $x1 + $x2 )/2;
             $ym = ( $y1 + $y2 )/2;
             $zm = ( $z1 + $z2 )/2;
-			
+
             $lom = atan2($ym , $xm);
             $hyp = sqrt($xm * $xm + $ym * $ym);
             $lam = atan2( $zm , $hyp);
-			
+
             $this->latitude = rad2deg($lam);
             $this->longitude = rad2deg($lom);
         }
@@ -81,9 +81,9 @@
         private $midpoint_airport_codes = array();
 
         public function __construct($location_1_lola, $location_2_lola, $radius)
-        {  
+        {
             $midpoint_info = $this->find_closest_midpoint($location_1_lola, $location_2_lola, $radius);
-           
+
             $this->midpoint_lola = $midpoint_info[0];
             $this->midpoint_airport_codes = $midpoint_info[1];
 
@@ -105,23 +105,23 @@
         {
             return ($this->midpoint_latitude);
         }
-        
+
         public function get_midpoint_airport_codes()
         {
             return ($this->midpoint_airport_codes);
         }
-        
+
         private function find_closest_midpoint($location_1_lola, $location_2_lola, $radius)
         {
             $mid_lola = NULL;
             $mid_airport_codes = array();
-          
+
             $mid_1 = new MidPoint_1($location_1_lola, $location_2_lola);
             $midpoint_1_airport_codes =  get_airport_codes($mid_1->get_lola(), $radius);
-            
+
             $mid_2 = new MidPoint_2($location_1_lola, $location_2_lola);
             $midpoint_2_airport_codes =  get_airport_codes($mid_2->get_lola(), $radius);
-      
+
             if(($midpoint_1_airport_codes == NULL) && ($midpoint_2_airport_codes != NULL))
             {
                 $mid_lola = $mid_2->get_lola();
@@ -148,7 +148,7 @@
                     $mid_airport_codes = $midpoint_2_airport_codes; // Both midpoint_1 and midpoint_2 airport codes may be either NULL or not NULL
                 }
             }
-      
+
             return (array($mid_lola, $mid_airport_codes));
         }
     }
